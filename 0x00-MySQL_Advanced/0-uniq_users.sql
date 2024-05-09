43120c60000f6c3eab9fea10e039eb4cb11da839
japheth-alt163 <japhethbaraka4@gmail.com>
2024-05-09 17:04:44 +0000

    my_SQL

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
+-- Create the users table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-);
+)
